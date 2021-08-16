1a0530a7213130dfd8dda05647ead81c5152843d
gkapitein <84490555+gkapitein@users.noreply.github.com>
2021-08-16 16:43:16 +0200

    Add files via upload

---
+++ Basic arrow functions/index.js
@@ -0,0 +1,13 @@
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+const fivePlusSeven = () => 5 + 7;
+let myFunction = (a, b) => a + b;
+let myFunctionB = a => a + 5;
+let createObject = () => ({ greeting: "hoi" });
+
+ikRockArrowFunctions()
+fivePlusSeven();
+
+createObject()
+
+ikRockArrowFunctions()
